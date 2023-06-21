153ae01aef0b09ec3721e6b767e7fa03e4e088b8
jacobtskidmore <jacobtskidmore@icloud.com>
2023-06-21 08:42:22 -0400

    Fixed issue with link hover incompatibility

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const navItem = document.querySelectorAll('.navItem a');
+var focus;
 
 navItem.forEach( elem => elem.addEventListener('mouseover', e => {
-  var focus = e.relatedTarget;
+  focus = e.fromElement;
   focus.classList.add('dark');
 }));
 
 navItem.forEach( elem => elem.addEventListener('mouseout', e => {
-  var focus = e.relatedTarget;
   focus.classList.remove('dark');
 }));
+
