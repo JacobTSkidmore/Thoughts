ed14bb94e72924c3e5a585da3a90a63d6b4607d4
jacobtskidmore <jacobtskidmore@icloud.com>
2023-06-23 16:08:47 -0400

    Added index link on all pages

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const navItem = document.querySelectorAll('.navItem a');
+const homeButton = document.querySelector('.logo a');
 var focus;
 
 navItem.forEach( elem => elem.addEventListener('mouseover', e => {
@@ -10,3 +11,10 @@ navItem.forEach( elem => elem.addEventListener('mouseout', e => {
   focus.classList.remove('dark');
 }));
 
+homeButton.addEventListener('mouseover', e => {
+  e.target.parentElement.classList.add('dark');
+});
+
+homeButton.addEventListener('mouseout', e => {
+  e.target.parentElement.classList.remove('dark');
+});
