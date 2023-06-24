809537dafbdb44301b80b5329f9f4a0b940b77ef
jacobtskidmore <jacobtskidmore@icloud.com>
2023-06-23 21:56:24 -0400

    Login page functional

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function login() {
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
 
-  if (username == "Karmic" && password == "Haylie011822?!") {
+  if (username == "admin" && password == "HiddenPassword123") {
     window.location.replace("post.html");
   }
   else {
