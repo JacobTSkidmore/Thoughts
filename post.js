e6072e3bf5e602ff2106dc3e57f47f17d3b084f5
jacobtskidmore <jacobtskidmore@icloud.com>
2023-06-23 22:53:59 -0400

    Post page wip, need php not js

---
+++ post.js
@@ -0,0 +1,6 @@
+function postBlog() {
+  const title = document.getElementById('postTitle').value;
+  const content = document.getElementById('postText').value;
+  const date = document.getElementById('postDate').value;
+  const topic = document.getElementById('postTopic').value;
+}
